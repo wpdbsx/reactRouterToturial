09d8ff3edb45baf94ba16a94fe585d5884b782b3
wpdbsx <wpdbsx@naver.com>
2020-02-14 13:58:00 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Link } from "react-router-dom";
 import About from "./About";
 import Home from "./Home";
+import Profiles from "./Profiles";
+import HistroySample from "./HistroySample";
+
 const App = () => {
   return (
     <div>
+      <ul>
+        <li>
+          <Link to="/">홈</Link>
+        </li>
+        <li>
+          <Link to="/about">소개</Link>
+        </li>
+        <li>
+          <Link to="/profiles">프로필</Link>
+        </li>
+        <li>
+          <Link to="/history">History 예제</Link>
+        </li>
+      </ul>
       <Route path="/" component={Home} exact={true} />
-      <Route path="/about" component={About} />
+      <Route path={["/about", "/info"]} component={About} />
+      <Route path="/profiles/" component={Profiles} />
+      <Route path="/history/" component={HistroySample} />
     </div>
   );
 };
